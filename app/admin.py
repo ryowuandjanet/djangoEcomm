3abab8f50ec2e4aa2b9274b39decda568bf9b5d1
ryowuandjanet <ryowu0329@gmail.com>
2023-07-01 21:16:01 +0800

    First Commit

---
+++ app/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from .models import Product,Customer
+
+@admin.register(Product)
+class ProductModelAdmin(admin.ModelAdmin):
+    list_display = ['id','title','discounted_price','category','product_image']
+
+@admin.register(Customer)
+class CustomerModelAdmin(admin.ModelAdmin):
+    list_display = ['id','user','name','locality','city','mobile','zipcode']
---
+++ app/models.py
@@ -0,0 +1,43 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+CATEGORY_CHOICES = (
+    ('CR','Curd'),
+    ('ML','Milk'),
+    ('LS','Lassi'),
+    ('MS','Milkshake'),
+    ('PN','Paneer'),
+    ('GH','Ghee'),
+    ('CZ','Cheese'),
+    ('IC','Ice-Creams')
+)
+
+STATE_CHOICES =(
+   ('Taipei','Taipei'),
+   ('Taichaung','Taichaung'),
+   ('Tainan','Tainan'),
+)
+
+class Product(models.Model):
+    title = models.CharField(max_length=100)
+    selling_price = models.FloatField()
+    discounted_price = models.FloatField()
+    description = models.TextField()
+    composition = models.TextField(default='')
+    prodapp = models.TextField(default='')
+    category=models.CharField(choices=CATEGORY_CHOICES, max_length=2)
+    product_image = models.ImageField(upload_to='product')
+
+    def __str__(self):
+      return self.title
+class Customer(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    locality = models.CharField(max_length=200)
+    city=models.CharField(max_length=50)
+    mobile = models.IntegerField(default=0)
+    zipcode = models.IntegerField()
+    state=models.CharField(choices=STATE_CHOICES, max_length=100)
+
+    def __str__(self):
+      return self.name
